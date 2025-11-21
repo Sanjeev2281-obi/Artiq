ca0a0d3dfec1863060a4e0fc3dfea7b8f16f6c93
Sanjeev2281-obi <sanjeevasas2281@gmail.com>
2025-11-21 23:31:19 +0530

    vikkisag

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Nav from './component/Nav'
 import Home from './pages/Main'
 import WorkshopBooking from './pages/WorkshopBooking'
 
