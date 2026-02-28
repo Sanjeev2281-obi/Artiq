93637f9ab20b52ba0cb99526519564da612279b9
Sanjeev <sanjeevasas2281@gmail.com>
2026-02-28 14:13:28 +0530

    bug fixed

--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -10,10 +10,10 @@ function Hero() {
                 className="w-full max-w-xs md:max-w-md lg:max-w-lg h-auto rounded-lg object-cover"
             />
             <p
-                className="flex-col p-2 text-[15px] md:text-[16px] -ml-[70px] "
+                className="flex-col p-2 text-[15px] md:text-[16px] -ml-17.5 "
             >
                Artiq Studio <br />
-               <span className="flex ml-[-50px] mt-1">Creating impact with Art</span>
+               <span className="flex -ml-12.5 mt-1">Creating impact with Art</span>
             </p>
         </div>
     )
